c86d078742340403f574a4143f398b3fa24c63ee
tariqhossain <tariq.hossain@digital.justice.gov.uk>
2025-12-07 19:18:46 +0000

    price change

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -129,7 +129,7 @@ export const features = [
       id: "1",
       title: "Overdrive",
       priceMonthly: 79,
-      priceYearly: 49,
+      priceYearly: 53,
       caption: "Most popular plan",
       features: [
         "All Starter features",
